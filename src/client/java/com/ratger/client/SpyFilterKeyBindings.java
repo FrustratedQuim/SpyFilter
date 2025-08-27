e0c327dc6517a96f2ada486ed00a0f8adc617009
QuimQuim <163081753+FrustratedQuim@users.noreply.github.com>
2025-08-27 21:39:20 +0700

    Update SpyFilterKeyBindings.java

--- src/client/java/com/ratger/client/SpyFilterKeyBindings.java
+++ src/client/java/com/ratger/client/SpyFilterKeyBindings.java
@@ -3,11 +3,8 @@ package com.ratger.client;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SpyFilterKeyBindings {
-    private static final Logger LOGGER = LoggerFactory.getLogger("spyfilter");
 
     private static final KeyBinding SPY_CHAT_TOGGLE_KEY = new KeyBinding(
             "key.spyfilter.toggle_spy_chat",
@@ -62,20 +59,14 @@ public class SpyFilterKeyBindings {
     }
 
     public static void toggleBasicSpy() {
-        LOGGER.info("Изменяем обычный чат. Старое значение: {}, новое значение: {}",
-                isChatSpyVisible, !isChatSpyVisible);
         isChatSpyVisible = !isChatSpyVisible;
     }
 
     public static void toggleSpyBook() {
-        LOGGER.info("Изменяем SPY-BOOK. Старое значение: {}, новое значение: {}",
-                isBookSpyVisible, !isBookSpyVisible);
         isBookSpyVisible = !isBookSpyVisible;
     }
 
     public static void toggleSpySign() {
-        LOGGER.info("Изменяем SPY-SIGN. Старое значение: {}, новое значение: {}",
-                isSignSpyVisible, !isSignSpyVisible);
         isSignSpyVisible = !isSignSpyVisible;
     }
 
