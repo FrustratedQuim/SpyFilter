b3e60e1fdce950fe056ff7f45baa10b98a52ea2f
FrustratedQuim <pokionch@gmail.com>
2026-03-09 17:52:24 +0700

    1.3.0

--- src/main/java/com/ratger/SpyFilter.java
+++ src/main/java/com/ratger/SpyFilter.java
@@ -1,14 +1,10 @@
 package com.ratger;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SpyFilter implements ModInitializer {
-	private static final Logger LOGGER = LoggerFactory.getLogger("spyfilter");
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("SpyFilter загружен!");
 	}
 }
